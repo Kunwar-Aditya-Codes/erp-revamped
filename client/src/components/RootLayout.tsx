8ae200624799b95653cd760f7601e305a26c6919
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-09-12 06:11:33 +0530

    added: login hero

--- client/src/components/RootLayout.tsx
+++ client/src/components/RootLayout.tsx
@@ -5,7 +5,7 @@ interface RootLayoutProps {}
 
 const RootLayout: FC<RootLayoutProps> = ({}) => {
   return (
-    <div className='h-screen  w-full  bg-[#161616] text-white'>
+    <div className='h-screen overflow-hidden w-full  bg-[#161616] text-white'>
       <Outlet />
     </div>
   );
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,9 +4,15 @@ interface LoginProps {}
 
 const Login: FC<LoginProps> = ({}) => {
   return (
-    <div className='flex h-full'>
-      <img src='' alt='' className='flex-[0.5]' />
-      <div className='flex-[0.5] flex items-center justify-center'>hello</div>
+    <div className='flex flex-col lg:flex-row h-full'>
+      <div className='bg-black/70 w-full flex items-center justify-center lg:justify-start  flex-[0.5] bg-center bg-cover bg-no-repeat bg-blend-color bg-[url("https://images.unsplash.com/photo-1581362072978-14998d01fdaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1884&q=80")]'>
+        <h1 className='text-5xl md:text-7xl text-center lg:ml-6 lg:w-[10rem] lg:text-8xl font-bold uppercase tracking-wider text-focus-in backdrop-blur-sm bg-white/20 rounded-sm p-4 lg:p-0'>
+          erp system
+        </h1>
+      </div>
+      <div className='flex-[0.5] w-full flex items-center justify-center'>
+        hello
+      </div>
     </div>
   );
 };
